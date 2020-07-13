a178da744ed08b12998abc04159ae36a22ee6ef9
SaemjuSupansa <6006021611062@fitm.kmutnb.ac.th>
2020-07-14 00:01:03 +0700

    test

---
+++ New folder/6006021611062_homework_chapter2/6006021611062_homework_chapter2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	cout << "1 st Trem of 2020" << "\" 6006021611062 Supansa Kaewsompak \"\n";
+	cout << "\"-------------------------------------------------------------\"\n";
+	cout << "\"| Subject ID\  | Subject name            \  | Score (100)\  | Grad |\"\n";
+	cout << "\"-------------------------------------------------------------\"\n";
+	cout << "\"| 4570034    \ | Introducation to computer\ |     85       \  | A |\"\n";
+	cout << "\"| 4580247    \ | English                  \ |     74       \  | B |\"\n";
+	cout << "\"| 4580247    \ | Design Thinking          \ |     92       \  | A |\"\n";
+	cout << "\"-------------------------------------------------------------\"\n";
+	cout << "\"| TOTAL      \ | Subject = 3              \ | Average = 3.87 |\"\n";
+	cout << "\"=============================================================\"\n";
+	cout << "\"KMUTNB\"\t\n";
+	return 0;
+}
