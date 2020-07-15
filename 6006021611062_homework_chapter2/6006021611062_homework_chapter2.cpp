77d3029a87128a91664fbe3081fee54c80c8911b
SaemjuSupansa <6006021611062@fitm.kmutnb.ac.th>
2020-07-16 00:37:06 +0700

    commit homeworkchapter2-2

--- 6006021611062_homework_chapter2/6006021611062_homework_chapter2.cpp
+++ 6006021611062_homework_chapter2/6006021611062_homework_chapter2.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main()
 {
 	cout << "1 st Trem of 2020" << "\" 6006021611062 Supansa Kaewsompak \"\n";
-	cout << "\"-------------------------------------------------------------\"\n";
-	cout << "\"| Subject ID\  | Subject name            \  | Score (100)\  | Grad |\"\n";
-	cout << "\"-------------------------------------------------------------\"\n";
-	cout << "\"| 4570034    \ | Introducation to computer\ |     85       \  | A |\"\n";
-	cout << "\"| 4580247    \ | English                  \ |     74       \  | B |\"\n";
-	cout << "\"| 4580247    \ | Design Thinking          \ |     92       \  | A |\"\n";
-	cout << "\"-------------------------------------------------------------\"\n";
-	cout << "\"| TOTAL      \ | Subject = 3              \ | Average = 3.87 |\"\n";
-	cout << "\"=============================================================\"\n";
-	cout << "\"KMUTNB\"\t\n";
+	cout << "-------------------------------------------------------------\n";
+	cout << "| Subject ID\t | Subject name\t | Score (100)\t | Grad |\t\n";
+	cout << "-------------------------------------------------------------\t\n";
+	cout << "| 4570034\t | Computer Programming\t | 85\t | A |\t\n";
+	cout << "| 4580247\t | English for work\t | 74\t | B |\t\n";
+	cout << "| 4580247\t | Design Thinking\t | 92\t | A |\t\n";
+	cout << "-------------------------------------------------------------\t\n";
+	cout << "| TOTAL\t | Subject = 3\t | Average = 3.87 |\t\n";
+	cout << "=============================================================\t\n";
+	cout << "\tKMUTNB\t\n";
 	return 0;
 }
